1a07e65e108cdfdf3f53219f2b31ec35906dfcc9
Kenta57 <0121kenta@gmail.com>
2022-01-15 09:33:49 +0000

    重ねて出力できるように変更

--- src_python/FFT.py
+++ src_python/FFT.py
@@ -93,40 +93,68 @@ def plot_stack(target_name, save_dir_name):
     plt.savefig(str(base_path / f'{target_name}.png'))
     plt.clf()
 
+def compare_group(target_name, condition, save_dir_name):
+    base_path = ROOT / 'result' / save_dir_name
+    path_list = [base_path / (target_name + f'_{i}' + condition) for i in range(2,5,1)]
 
-def compare_same(name):
-    path_list = [ROOT / 'result' / name / (name+f'-flw{i}-rtt.data') for i in range(3)]
     for p in path_list:
-        main(p)
-
-        # print(fft_spec.shape)
-
-def compare():
-    save_path = ROOT / 'result' / 'FFT_SR_800'
-    l = [
-        # 'gw_30_normal_2_range10',
-        # 'normal_2',
-        'udp_2_range10',
-        'error_0001_2_range10'
-    ]
-    # l = [
-    #     'gw_30_normal_2_range50',
-    #     'normal_2_range50',
-    #     'udp_2_range50',
-    #     'error_0001_2_range50'
-    # ]
-    
-    for name in l:
-        path = save_path / (name+'-flw0-rtt.npy')
-        x = np.load(path)
+        x = np.load(p)
+        n = len(x)
+        x = x[int(n/6):]
         size_frame = len(x)
         hamming_window = np.hamming(size_frame)
         fft_spec = np.fft.rfft(x * hamming_window)
-        ans = np.log(np.abs(fft_spec))
-        plt.plot(ans[2:100],label=name)
+        ans = np.abs(fft_spec)
+        # ans = np.log(np.abs(fft_spec))
+        plt.plot(ans[2:100],label=p.stem)
         plt.legend()
 
-    plt.savefig(str(save_path / ('range10_SR_800_udp_error.png')))
+    base_path = ROOT / 'result' / 'test2'
+    base_path.mkdir(exist_ok=True)
+
+    save_path = base_path / 'figure'
+    save_path.mkdir(exist_ok=True)
+    plt.savefig(str(save_path / (f'{target_name}_{condition}.png')))
+
+def compare(condition, save_dir_name):
+    range10_list = [
+        'error_0001_2_range10-flw0-rtt.npy',
+        'gw_30_normal_2_range10-flw0-rtt.npy',
+        'normal_2-flw0-rtt.npy',
+        'udp_2_range10-flw0-rtt.npy'
+    ]
+    range50_list = [
+        'error_0001_2_range50-flw0-rtt.npy',
+        'gw_30_normal_2_range50-flw0-rtt.npy',
+        'normal_2_range50-flw0-rtt.npy',
+        'udp_2_range50-flw0-rtt.npy'
+    ]
+    if condition == 10:
+        target_list = range10_list
+    elif condition == 50:
+        target_list = range50_list
+
+    base_path = ROOT / 'result' / save_dir_name
+    path_list = [base_path / f_name for f_name in target_list]
+
+    for p in path_list:
+        x = np.load(p)
+        n = len(x)
+        x = x[int(n/6):]
+        size_frame = len(x)
+        hamming_window = np.hamming(size_frame)
+        fft_spec = np.fft.rfft(x * hamming_window)
+        ans = np.abs(fft_spec)
+        # ans = np.log(np.abs(fft_spec))
+        plt.plot(ans[2:100],label=p.stem)
+        plt.legend()
+
+    base_path.mkdir(exist_ok=True)
+
+    save_path = base_path / 'figure'
+    save_path.mkdir(exist_ok=True)
+    plt.savefig(str(save_path / (f'{condition}.png')))
+    plt.clf()
 
 
 
@@ -145,34 +173,41 @@ if __name__ == '__main__':
     # path_list = [Path(p) for p in path_list]
     
     l = [
-        'error_0001_2_range10',
-        'error_0001_2_range50',
-        # 'error_0001_3_range10',
-        # 'error_0001_3_range50',
-        # 'error_0001_4_range10',
-        # 'error_0001_4_range50',
-        'gw_30_normal_2_range10',
-        'gw_30_normal_2_range50',
-        # 'gw_30_normal_3_range10',
-        # 'gw_30_normal_3_range50',
-        # 'gw_30_normal_4_range10',
-        # 'gw_30_normal_4_range50',
-        'normal_2',
-        'normal_2_range50',
-        # 'normal_3',
-        # 'normal_3_range50',
-        # 'normal_4',
-        # 'normal_4_range50',
-        'udp_2_range10',
-        'udp_2_range50',
-        # 'udp_3_range10',
-        # 'udp_3_range50',
-        # 'udp_4_range10',
-        # 'udp_4_range50'
+        # 'error_0001_2_range10',
+        # 'error_0001_2_range50',
+        'error_0001_3_range10',
+        'error_0001_3_range50',
+        'error_0001_4_range10',
+        'error_0001_4_range50',
+        # 'gw_30_normal_2_range10',
+        # 'gw_30_normal_2_range50',
+        'gw_30_normal_3_range10',
+        'gw_30_normal_3_range50',
+        'gw_30_normal_4_range10',
+        'gw_30_normal_4_range50',
+        # 'normal_2',
+        # 'normal_2_range50',
+        'normal_3',
+        'normal_3_range50',
+        'normal_4',
+        'normal_4_range50',
+        # 'udp_2_range10',
+        # 'udp_2_range50',
+        'udp_3_range10',
+        'udp_3_range50',
+        'udp_4_range10',
+        'udp_4_range50'
     ]
     
     # target_name = 'gw_30_normal_2_range10'
-    for target_name in l:
-        save_dir_name = 'test'
-        sampling(target_name, save_dir_name)
-        plot_stack(target_name,save_dir_name)
+    # for target_name in l:
+    #     save_dir_name = 'test'
+    #     sampling(target_name, save_dir_name)
+    #     plot_stack(target_name,save_dir_name)
+
+    # target_name = 'normal'
+    # condition = '_range50-flw0-rtt.npy'
+    # save_dir_name = 'test'
+    # compare_group(target_name, condition, save_dir_name)
+
+    compare(50,'test')
