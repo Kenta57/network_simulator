d890dbf41ddc29f88dfc9cb169649c259aec466a
Kenta57 <0121kenta@gmail.com>
2022-04-17 08:13:57 +0000

    plot関数の追加

--- src_python/RTT.py
+++ src_python/RTT.py
@@ -3,109 +3,20 @@ from pathlib import Path
 from tqdm import tqdm
 from collections import deque
 
+import plot
+import matplotlib.pyplot as plt
+
 ROOT = Path.cwd().parent
 
-class Extractor:
-    def __init__(self, target_path, sack_option=True, prefix='inflight'):
-        self.cap = pyshark.FileCapture(str(target_path))
-        self.prefix = prefix
-        self.save_dir = target_path.parent
-        self.sack_option = sack_option
-        self.save_paths = [self.save_dir / f'{self.save_dir.name}-sack-{self.sack_option}-flw{i}-{self.prefix}.data' for i in range(3)]
-        for p in self.save_paths:
-            p.unlink(missing_ok=True)
-
-        self.clean_file()
-
-        self.f_streams = [open(str(path), mode='a') for path in self.save_paths]
-        self.highest_ack = [0]*3
-        self.inflight = [0]*3
-        self.DUP_ACK_cnt = [0]*3
-        self.ACK = [0]*3
-
-    def __del__(self):
-        self.output_DUP_ACK()
-        for f in self.f_streams:
-            f.close()
-
-    def clean_file(self):
-        for p in self.save_paths:
-            p.unlink(missing_ok=True)
-            p.touch(exist_ok=True)
-
-    def extract(self):
-        for i, packet in tqdm(enumerate(self.cap)):
-            transport_layer = packet.transport_layer
-            if transport_layer == 'TCP':
-                stream_idx = int(packet.tcp.stream)
-                if stream_idx > 2:
-                    continue
-                self.get_inflight(packet, stream_idx)
-                self.count_DUP_ACK(packet, stream_idx)
-
-    def count_DUP_ACK(self, packet, stream_idx):
-        ack = int(packet.tcp.ack)
-        seq = int(packet.tcp.seq)
-        if seq == 1 and not(ack == 1):
-            if ack == self.ACK[stream_idx]:
-                self.DUP_ACK_cnt[stream_idx] += 1
-            else:
-                self.ACK[stream_idx] = ack
-
-    def get_inflight(self, packet, stream_idx):
-        seq = int(packet.tcp.seq)
-        ack = int(packet.tcp.ack)
-        nxt_seq = int(packet.tcp.nxtseq)
-        if seq == 1 and not(ack == 1):
-            if self.sack_option:
-                try:
-                    sack_option_raw = packet.tcp.options_sack
-                    ack_virtual = self.get_virtual_ack(sack_option_raw, ack)
-                    self.highest_ack[stream_idx] = ack_virtual
-                except AttributeError:
-                    self.highest_ack[stream_idx] = max(ack, self.highest_ack[stream_idx])
-            else:
-                self.highest_ack[stream_idx] = max(ack, self.highest_ack[stream_idx])
-        if not(seq == 1) and ack == 1:
-            highest_nxt_seq = nxt_seq
-            __inflight = highest_nxt_seq - self.highest_ack[stream_idx]
-            self.inflight[stream_idx] = __inflight if __inflight > 0 else self.inflight[stream_idx]
-            time = packet.sniff_timestamp
-            self.f_streams[stream_idx].write(f'{time} {self.inflight[stream_idx]}\n')
-
-    def get_virtual_ack(self, byte_raw, ack):
-        sack = self.get_sack_list(byte_raw)
-        sack_cnt = len(sack)//2
-        received = ack - 1
-        for i in range(sack_cnt):
-            received += sack[2*i+1] - sack[2*i]
-        return received+1
-
-    def get_sack_list(self, byte_raw):
-        data = byte_raw.split(':')
-        kind, length = data[0:2]
-        data = data[2:]
-        n = len(data)//4
-        __sack = [int(''.join(data[4*i+1:4*i+4]),16) for i in range(n)]
-        sack_cnt = len(__sack)//2
-        sack = []
-        for i in range(sack_cnt)[::-1]:
-            sack += __sack[2*i:2*i+2]
-        return sack
-
-    def output_DUP_ACK(self):
-        save_path = self.save_dir / f'{self.save_dir.name}-DUP_ACK_num.data'
-        with open(str(save_path), mode='w') as f:
-            for i, v in enumerate(self.DUP_ACK_cnt):
-                f.write(f'{i} {v}\n')
-
 class rtt_estimator:
-    def __init__(self, target_path):
+    def __init__(self, target_path, save_path):
         self.alpha = 0.125
-        # self.beta =0.25
         self.estimatedRtt = None
         self.samplingRtt = None
         self.cap = pyshark.FileCapture(str(target_path))
+        paths = [save_path / 'rtt_sampling.data', save_path / 'rtt_estimate.data']
+
+        self.streams = [open(str(p), mode='a') for p in paths]
 
     def isACK(self, segment):
         seq = int(segment.seq)
@@ -130,16 +41,16 @@ class rtt_estimator:
                         continue
                     else:
                         TSval = ack_queue[0].tcp.options_timestamp_tsval
-                        self.rtt_sampling(ack_queue[0], packet, index)
+                        self.rtt_sampling(ack_queue[0], packet)
                         self.rtt_estimate()
                         ack_queue.popleft()
                         # TODO: RTT_sampling
 
-    def rtt_sampling(self, packet_TSval, packet_TSecr, index):
-        self.samplingRtt = float(packet_TSecr.sniff_timestamp) - float(packet_TSval.sniff_timestamp) 
-        # print(f'TSval : {packet_TSval.tcp.options_timestamp_tsval}')
-        # print(f'TSecr : {packet_TSecr.tcp.options_timestamp_tsecr}')
-        print(f'rtt_s : {self.samplingRtt}, index : {index}')
+    def rtt_sampling(self, packet_TSval, packet_TSecr):
+        self.samplingRtt = float(packet_TSecr.sniff_timestamp) - float(packet_TSval.sniff_timestamp)
+        self.time = packet_TSecr.sniff_timestamp
+        self.streams[0].write(f'{self.time} {self.samplingRtt}\n')
+        print(f'time : {self.time}, rtt_s : {self.samplingRtt}')
         return self.samplingRtt
 
     def rtt_estimate(self):
@@ -147,9 +58,51 @@ class rtt_estimator:
             self.estimatedRtt = self.samplingRtt
         else:
             self.estimatedRtt += (self.samplingRtt - self.estimatedRtt) * self.alpha
-        print(f'rtt_e : {self.estimatedRtt}')
+        print(f'time : {self.time}, rtt_e : {self.estimatedRtt}')
+        self.streams[1].write(f'{self.time} {self.estimatedRtt}\n')
         return self.estimatedRtt
 
+def plot_rtt(target_path):
+    metric = plot.read_data(file_name = target_path, duration = 30)
+
+    # plt.title(name[:-10])
+    plt.subplots_adjust(left=0.2)
+    x_ticks = True
+    x_max = None
+    y_max = None
+    para = 'rtt'
+    y_label = para
+    if para=='inflight' or para=='cwnd':
+        y_max = 250000
+
+    y_deno = 1
+    # 階段状にplotする
+    # where=preは左側にstepができる
+    # cは色でkは黒
+    plt.step(
+        metric.sec, metric.value/y_deno,
+        # c='k', 
+        where='pre')
+    # x方向の表示範囲
+    plt.xlim(0, x_max)
+    # y軸のラベル名
+    plt.ylabel(y_label.upper()+'[s]')
+
+    # y軸の最大値．
+    if y_max:
+        plt.ylim(0, y_max)
+
+    # x軸のメモリを表示するか否か．
+    if x_ticks:
+        plt.xlabel('time[s]')
+    else:
+        plt.xticks([])
+
+    save_path = ROOT / f'src_python/{target_path.stem}.png'
+
+    # 保存
+    plt.savefig(str(save_path))
+    plt.clf()
                 
 
 
@@ -161,10 +114,13 @@ if __name__ == '__main__':
     # del Ex
 
     target_path = Path('/home/murayama/Document/ns3/ns-3-allinone/ns-3.30/data/Normal_0_range10/Normal_0_range10-1-1.pcap')
-    # cap = pyshark.FileCapture(str(target_path))
-    # test(cap)
-    r_estimator = rtt_estimator(target_path)
-    r_estimator.test()
+    save_path = Path('/home/murayama/Document/ns3/ns-3-allinone/ns-3.30/src_python')
+    # r_estimator = rtt_estimator(target_path, save_path)
+    # r_estimator.test()
+
+    path_list = [save_path / 'rtt_samplig.data', save_path / 'rtt_estimate.data']
+    for p in path_list:
+        plot_rtt(p)
 
 
 
